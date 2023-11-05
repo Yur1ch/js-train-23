4901998dfe1b74cae34e3521588c21c8c341c145
Yur1ch <yuraboxi56@gmail.com>
2023-11-05 21:10:48 +0200

    hw23

--- 8.js
+++ 8.js
@@ -1,33 +1,57 @@
-// Медіатор (Mediator) — це патерн програмування, який визначає об'єкт, який інкапсулює взаємодію між групою об'єктів. Медіатор сприяє слабкій залежності між цими об'єктами,
-// дозволяючи спілкуватися з ними через централізовану точку.
-
-// Клас User відповідає за користувача, який може відправляти повідомлення.
 class User {
-  // Створюємо конструктор класу, який приймає name та messenger як параметри та ініціалізує їх
-  // Робимо метод sendMessage який відправляє повідомлення за допомогою відповідного месенджера, та виводить в консоль `${this.name} відправив повідомлення ${message}`.
-  // Він приймає один параметр - message - повідомлення, яке потрібно відправити за допомогою методу sendMessage.
-  // Метод receiveMessage приймає аргументи user,message та виводить в консоль ${this.name} отримав повідомлення від ${user.name}: ${message}
+  constructor(name, mediator) {
+    this.name = name;
+    this.mediator = mediator;
+  }
+
+  sendMessage(message) {
+    this.mediator.sendMessage(this, message);
+  }
+
+  receiveMessage(user, message) {
+    console.log(
+      `${this.name} отримав повідомлення від ${user.name}: ${message}`
+    );
+  }
 }
 
-// Клас SMSMessenger відповідає за відправку повідомлень за допомогою SMS.
 class SMSMessenger {
-  // Створюємо статичний метод sendMessage який приймає один параметр - message, та виводить в консоль `Відправлено SMS: ${message}`
+  static sendMessage(message) {
+    console.log(`Відправлено SMS: ${message}`);
+  }
 }
 
-// Клас EmailMessenger відповідає за відправку повідомлень за допомогою Email.
 class EmailMessenger {
-  // Створюємо статичний метод sendMessage який приймає один параметр - message, та виводить в консоль `Відправлено Email: ${message}`
+  static sendMessage(message) {
+    console.log(`Відправлено Email: ${message}`);
+  }
+}
+
+class Mediator {
+  sendMessage(user, message) {
+    if (user.mediator === SMSMessenger) {
+      SMSMessenger.sendMessage(`[${user.name}]: ${message}`);
+    } else if (user.mediator === EmailMessenger) {
+      EmailMessenger.sendMessage(`[${user.name}]: ${message}`);
+    }
+
+    users.forEach((otherUser) => {
+      if (otherUser !== user) {
+        otherUser.receiveMessage(user, message);
+      }
+    });
+  }
 }
 
 console.log("Завдання 7 ====================================");
-// Після виконання розкоментуйте код нижче
 
-// Створюємо двох користувачів - John та Jane - які відправляють повідомлення за допомогою різних месенджерів.
-// const john = new User("John", SMSMessenger);
-// const jane = new User("Jane", EmailMessenger);
+const john = new User("John", SMSMessenger);
+const jane = new User("Jane", EmailMessenger);
 
-// John відправляє повідомлення за допомогою SMS.
-// john.sendMessage("Привіт!"); // Виведе: Відправлено SMS: [John]: Привіт!
+const mediator = new Mediator();
+const users = [john, jane];
 
-// Jane відправляє повідомлення за допомогою Email.
-// jane.sendMessage("Привіт!"); // Виведе: Відправлено Email: [Jane]: Привіт!
+john.sendMessage("Привіт!");
+
+jane.sendMessage("Привіт!");
+
